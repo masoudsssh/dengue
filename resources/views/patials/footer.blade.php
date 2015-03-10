194f4c064de8489237e3bc16fabb82b73fd15813
masoud <masoud@itechspark.com.my>
2015-03-10 17:22:46 +0800

    minor fix

--- resources/views/patials/footer.blade.php
+++ resources/views/patials/footer.blade.php
@@ -6,22 +6,22 @@ Copyright &copy; 2015 Komuniti Bebas Denggi. All rights reserved.
 </div><!-- end boxed_wrapper -->
 </div><!-- end site_wrapper -->
 
-<script src="/lib/jquery/dist/jquery.min.js" type="text/javascript"></script>
-<script src="/lib/datatables/media/js/jquery.dataTables.min.js"></script>
-<script src="/lib/angular/angular.min.js"></script>
-<script src="/lib/angular-datatables/dist/angular-datatables.min.js"></script>
+<script src="lib/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="lib/datatables/media/js/jquery.dataTables.min.js"></script>
+<script src="lib/angular/angular.min.js"></script>
+<script src="lib/angular-datatables/dist/angular-datatables.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true"></script>
 
-<script src="/lib/angular-route/angular-route.min.js"></script>
-<script src="/lib/ui-bootstrap/ui-bootstrap-tpls-0.6.0.js"></script>
-<script src="/lib/angular-resource/angular-resource.min.js"></script>
+<script src="lib/angular-route/angular-route.min.js"></script>
+<script src="lib/ui-bootstrap/ui-bootstrap-tpls-0.6.0.js"></script>
+<script src="lib/angular-resource/angular-resource.min.js"></script>
 
-<script src="/lib/angular-file-upload/angular-file-upload.js"></script> 
+<script src="lib/angular-file-upload/angular-file-upload.js"></script> 
 
-<script src="/js/app.js"></script>
-<script src="/js/route.js"></script>
-<script src="/js/models/case.js"></script>
-<script src="/js/controllers/caseController.js"></script>
+<script src="js/app.js"></script>
+<script src="js/route.js"></script>
+<script src="js/models/case.js"></script>
+<script src="js/controllers/caseController.js"></script>
 
 </body>
 </html>
