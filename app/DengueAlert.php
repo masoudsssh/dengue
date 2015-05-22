11a622eab16fc855358a13f27b7856d130005d04
masoud <masoud@itechspark.com.my>
2015-05-22 14:34:59 +0800

    minor update

--- app/DengueAlert.php
+++ app/DengueAlert.php
@@ -5,6 +5,6 @@ use Illuminate\Database\Eloquent\Model;
 class DengueAlert extends Model {
 
 	protected $table = 'dengue_alerts';
-	protected  $fillable = ['area', 'road_name'];
+	protected  $fillable = ['area', 'road_name', 'user_id'];
 
 }
