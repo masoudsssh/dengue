57f8abaa08250ae4458da98de3246b28ffe966ba
masoud <masoud@itechspark.com.my>
2015-06-02 22:34:57 +0800

    minor change 2

--- app/Http/Controllers/HotspotController.php
+++ app/Http/Controllers/HotspotController.php
@@ -125,7 +125,7 @@ class HotspotController extends Controller {
 		$hotspot->whereHas('hotspots', function($q){
 		    $q->where('no_of_cases', '!=', '0' );
 		});
-		return $hotspot->select('id', 'state', 'district', 'area', 'gps_location', 'hotspots')->groupBy('area')->get();
+		return $hotspot->groupBy('area')->get();
 	}
 
 
